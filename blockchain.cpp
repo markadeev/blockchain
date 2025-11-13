83e4ee58a5cb2d6aa63bf93c7733325156d1b56b
markadeev <markadeev@ukr.net>
2025-11-13 18:20:35 +0200

    added calculate hash with sha256

--- blockchain.cpp
+++ blockchain.cpp
@@ -2,22 +2,34 @@
 #include <string>
 #include <ctime>
 #include <vector>
+#include <openssl/sha.h>
+#include <iomanip>
 using namespace std;
 class Block {
 	public:
-		string prevBlockHash;
+		unsigned char prevBlockHash[32] = {0};
 		time_t timestamp;
-		int nonce;
-		string thisBlockHash;
+		int nonce = 0;
+		unsigned char thisBlockHash[32];
 		string MerkleRoot;
 		vector<string> transactions;
+
+		// calculate hash
+		unsigned char * calculateHash(){
+			return SHA256(prevBlockHash, 32, thisBlockHash);
+		}
 };
 int main(){
 	Block block1;
 	Block block2;
 	Block block3;
-	block2.prevBlockHash = block1.thisBlockHash;
-	block3.prevBlockHash = block2.thisBlockHash;
+
+	block1.calculateHash();
+
+	// print hash raw bytes in hex format
+	for (auto c : block1.thisBlockHash){
+		printf("%02x ", c);
+	}
 
 	return 0;
 }
